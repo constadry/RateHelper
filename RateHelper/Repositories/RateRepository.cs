220345cc1746e7a26fa5fd67c047ce3d53bfb394
constadry <behappydtworry@gmail.com>
2023-01-22 14:41:08 +0300

    feat: other ticker pairs

--- RateHelper/Repositories/RateRepository.cs
+++ RateHelper/Repositories/RateRepository.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
-using MongoDB.Bson;
 using MongoDB.Driver;
 using RateHelper.Models;
 
@@ -12,6 +9,7 @@ namespace RateHelper.Repositories
     public class RateRepository : IRateRepository
     {
         private readonly IMongoCollection<Rate> _rates;
+        private const string _primaryPair = "eth_usd";
         
         public RateRepository(IOptions<RateStoreDatabaseSettings> rateStoreDatabaseSettings)
         {
@@ -26,7 +24,8 @@ namespace RateHelper.Repositories
         public async Task<Rate> GetLastRate(DateTime updateDate)
         {
             // var filter = Builders<Rate>.Filter.Gte(x => x.UpdateDate, updateDate);
-            return await _rates.Find(x => x.UpdateDate >= updateDate)
+            return await _rates.Find(x => x.UpdateDate >= updateDate
+                                          && x.RateName == _primaryPair)
                 .SortBy(x => x.UpdateDate)
                 .Limit(1)
                 .SingleAsync();
@@ -34,7 +33,7 @@ namespace RateHelper.Repositories
 
         public async Task<Rate> GetLastRate()
         {
-            return await _rates.Find(x => true)
+            return await _rates.Find(x => x.RateName == _primaryPair)
                 .SortByDescending(x => x.UpdateDate)
                 .Limit(1)
                 .SingleAsync();
--- RateHelper/Services/RateParsingService.cs
+++ RateHelper/Services/RateParsingService.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ namespace RateHelper.Services
     {
         private readonly HttpClient _httpClient;
         private readonly IRateService _rateService;
+        private const int _oneSecond = 1000;
         
         public RateParsingService(HttpClient httpClient, IRateService rateService)
         {
@@ -20,29 +23,54 @@ namespace RateHelper.Services
 
         public async void Scan()
         {
+            const int timeout = 60000;
+            const string primaryPair = "eth_usd";
+            var pairNames = await GetPairNames();
+            var rnd = new Random();
+
             while (true)
             {
-                var rate = await GetOneRate();
-                InsertRateToDb(rate);
-                Thread.Sleep(60000);
+                // primary ticker
+                var rateEthUsd = await GetOneRate(primaryPair);
+                InsertRateToDb(rateEthUsd, primaryPair);
+
+                Thread.Sleep(_oneSecond);
+                
+                // other ticker (getting randomly to don't create a high load on api)
+                var tickerInd = rnd.Next(pairNames.Count - 1);
+                var tickerName = pairNames.ElementAtOrDefault(tickerInd);
+                var rate = await GetOneRate(tickerName);
+                InsertRateToDb(rate, tickerName);
+                Thread.Sleep(timeout);
             }
         }
 
-        private async Task<YobitRate> GetOneRate()
+        private async Task<List<string>> GetPairNames()
         {
-            var rateResponse = await _httpClient.GetAsync("https://yobit.net/api/3/ticker/eth_usd") 
-                               ?? throw new Exception("yobit.net doesn't send data");
-            var rateContent = await rateResponse.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<YobitRate>(rateContent);
+            var response = await _httpClient.GetAsync("https://yobit.net/api/3/info")
+                           ?? throw new Exception("yobit.net doesn't send data");
+            var json = await response.Content.ReadAsStringAsync();
+            var o = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+            var pairs = JsonConvert.DeserializeObject<Dictionary<string, object>>(o["pairs"].ToString());
+            Thread.Sleep(_oneSecond);
+            return pairs.Keys.ToList();
         }
 
-        private async void InsertRateToDb(YobitRate rate)
+        private async Task<dynamic> GetOneRate(string tickerName)
+        {
+            var rateResponse = await _httpClient.GetAsync($"https://yobit.net/api/3/ticker/{tickerName}") 
+                               ?? throw new Exception("yobit.net doesn't send data");
+            var rateContent = await rateResponse.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject(rateContent);
+        }
+
+        private async void InsertRateToDb(dynamic rate, string tickerName)
         {
             await _rateService.CreateRate(
                 new Rate
                 {
-                    RateName = "eth_usd",
-                    Value = rate.EthUsd.Last,
+                    RateName = tickerName,
+                    Value = (double)rate[tickerName]["last"],
                     UpdateDate = DateTime.Now,
                 });
         }
